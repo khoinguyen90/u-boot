f9b84130e2a15baa34f21489d2b2581122885811
Khoi Nguyen <k.van.nguyen@praxis-automation.com>
2024-01-11 10:35:09 +0000

    Add Praxis Linux TFT board

---
+++ include/configs/praxis-rk3568.h
@@ -0,0 +1,10 @@
+#ifndef __PRAXIS_RK3568_H
+#define __PRAXIS_RK3568_H
+
+#include <configs/rk3568_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+			"stdout=serial,vidconsole\0" \
+			"stderr=serial,vidconsole\0"
+
+#endif // __PRAXIS_RK3568_H
